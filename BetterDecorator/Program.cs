9989fbfd2c54ec7efb33b267f30482aa54c7ebc8
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-03-13 18:57:25 +0000

    Added XML comments to the methods

--- BetterDecorator/Program.cs
+++ BetterDecorator/Program.cs
@@ -3,14 +3,26 @@
 namespace BetterDecorator
 {
     class Program
-    {
+    {   /// <summary>
+        /// Write a string that contain a specific character that appears 
+        /// n times to the left and right of the phrase.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             //Prints the string modified by the Decor method
             Console.WriteLine(Decor(args[0],char.Parse(args[1])
             ,int.Parse(args[2])));
         }
-       
+        /// <summary>
+        /// Receive a string, a character and an integrable and make 
+        /// the character appear the same number of times as the integrable, to
+        /// the left and right of the string received.
+        /// </summary>
+        /// <param name="s">string to be received</param>
+        /// <param name="dec">character to be received</param>
+        /// <param name="n">integrable to be received</param>
+        /// <returns></returns>
         private static string Decor (string s, char dec, int n)
         {
             //Creates the final string
