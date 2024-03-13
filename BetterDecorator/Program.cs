cc19ac11e21f71689a97eb51e269a59e9def27a9
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-03-13 12:51:59 +0000

    Modified Decor method so it takes 3 arguments, functional

--- BetterDecorator/Program.cs
+++ BetterDecorator/Program.cs
@@ -6,11 +6,31 @@ namespace BetterDecorator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Prints the string modified by the Decor method
+            Console.WriteLine(Decor(args[0],char.Parse(args[1])
+            ,int.Parse(args[2])));
         }
-    }   
-        private static string Decor ( string s , char dec , int n)
+       
+        private static string Decor (string s, char dec, int n)
         {
-            return $"{dec}{dec}{dec} {s} {dec}{dec}{dec}";
+            //Creates the final string
+            string str = "";
+
+            for(int i=0; i<n; i++) 
+            //Adds character to the beginning of the final string while i < n
+            {
+                str += $"{dec}";
+            }
+            // Adds the string itself to the final string
+            str += $" {s} ";
+
+            for(int i=0; i<n; i++) 
+            //Adds character to the end of the final string while i < n
+            {
+                str += $"{dec}";
+            }
+            //Prints the final string
+            return (str);
         }
+    }
 }
