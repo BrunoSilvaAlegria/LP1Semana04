3d74366ed34cfa832da6a39391abe7cb68bdeb99
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-03-13 11:21:27 +0000

    Inserted the method Decor with an int reception

---
+++ BetterDecorator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BetterDecorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }   
+        private static string Decor ( string s , char dec , int n)
+        {
+            return $"{dec}{dec}{dec} {s} {dec}{dec}{dec}";
+        }
+}
