591eb4ed21717d78329fc4ed75baa9d7a3e24a90
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-03-14 17:46:10 +0000

    ex 4, added GetSpecial method and testing in Main

---
+++ Special/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Special
+{
+    class Program
+    {
+        private static int GetSpecial(int n)
+        {
+
+            int i = 0;
+            if (n == 1) //Base case
+                i = 1;
+            else
+                i = n * GetSpecial(n - 1); //Other possibilities
+            return i;
+        
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            GetSpecial(0);
+            GetSpecial(2);
+        }
+    }
+}
